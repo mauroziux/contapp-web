a9bcb3ec32a1b5602af59871de527d4fc4b8c1a1
mauricio <mauroziux@gmail.com>
2016-10-24 00:02:36 -0500

    update

--- resources/views/template/head.blade.php
+++ resources/views/template/head.blade.php
@@ -111,7 +111,8 @@
 
 <!-- END Stylesheets -->
 <!-- Web fonts -->
-<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400italic,600,700%7COpen+Sans:300,400,400italic,600,700">
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400italic,600,700%7COpen+Sans:300,400,400italic,600,700" media="none" onload="if(media!='all')media='all'">
+
 
 
 
