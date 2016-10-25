f8eaf499dbfacb14286fac2a424915bc2f46b15f
mauricio <mauroziux@gmail.com>
2016-10-24 23:26:42 -0500

    up

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -28,3 +28,5 @@
 <!-- END Stylesheets -->
 <!-- Web fonts -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400italic,600,700%7COpen+Sans:300,400,400italic,600,700" media="none" onload="if(media!='all')media='all'">
+<script src="assets/js/oneui.min.js"></script>
+<script src="assets/js/typed.min.js"></script>
--- resources/views/template/scripts.blade.php
+++ resources/views/template/scripts.blade.php
@@ -1,6 +1,6 @@
 <!-- OneUI Core JS: jQuery, Bootstrap, slimScroll, scrollLock, Appear, CountTo, Placeholder, Cookie and App.js -->
 <script src="assets/js/oneui.min.js"></script>
-<script  src="assets/js/typed.min.js" media="none" onload="if(media!='all')media='all'"></script>
+<script async  src="assets/js/typed.min.js"></script>
 <script>
 
   $(function(){
