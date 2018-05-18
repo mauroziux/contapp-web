9d193e43f35c08f74cb98181b90052a5e4fcfc0f
Mauricio Suárez vega <mauroziux@gmail.com>
2018-05-17 22:47:19 -0500

    fix

--- resources/views/emails/new.blade.php
+++ resources/views/emails/new.blade.php
@@ -1,1 +1,6 @@
 <div><b>Nombre: </b>{{$data->name}}</div>
+<div><b>email: </b>{{$data->email}}</div>
+<div><b>Telefono: </b>{{$data->telefono}}</div>
+<div><b>Empresa: </b>{{$data->empresa}}</div>
+<div><b>Ciudad: </b>{{$data->ciudad}}</div>
+<div><b>Mensaje: </b>{{$data->mensaje}}</div>
